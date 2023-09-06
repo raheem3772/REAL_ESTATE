6f6c8954d427388582f0dad4d0210812b5a33487
Sameer Liaqat <40518031+raheem3772@users.noreply.github.com>
2023-09-06 13:56:20 -0700

    Revert "Revert "fix all issues""
    This reverts commit 28250bdc7a7a12a3a224bc829cb04cfd6d92d245.

--- frontend/backend/routes/review.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ReviewController = require("../controllers/reviewController");
-
-// Routes
-router.get("/", ReviewController.getReview);
-router.post("/", ReviewController.addReview);
-
-module.exports = router;
