04dcd1d758c08d2cc922206227f94d073f24c548
Sameer Liaqat <59957710+sameerliaqat00@users.noreply.github.com>
2023-09-06 13:24:24 -0700

    fix all issues

--- frontend/backend/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const AdminController = require("../controllers/adminController");
-
-// Routes
-router.post("/", AdminController.addAdmin);
-
-module.exports = router;
