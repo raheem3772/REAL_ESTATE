ff77116261a91474103107f81ec80b3ec269d8ca
Muhammad Raheem <40518031+raheem3772@users.noreply.github.com>
2023-09-06 14:10:17 -0700

    Delete backend directory

--- backend/routes/comments.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const CommentController = require("../controllers/commentsController");
-
-// Routes
-router.get("/", CommentController.getAllComments);
-router.post("/", CommentController.addComments);
-
-module.exports = router;
--- backend/routes/review.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ReviewController = require("../controllers/reviewController");
-
-// Routes
-router.post("/", ReviewController.addReview);
-router.get("/", ReviewController.getReview);
-
-module.exports = router;
